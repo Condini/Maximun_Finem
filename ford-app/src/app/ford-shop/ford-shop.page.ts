025b70ac1b84bbef4a9c6320f94357d082f163a5
Guilherme Condini <47857069+Condini@users.noreply.github.com>
2020-11-05 19:21:59 -0300

    Alteraçoes fordshop e css do fordpass
    commitson

--- ford-app/src/app/ford-shop/ford-shop.page.ts
+++ ford-app/src/app/ford-shop/ford-shop.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: 'app-ford-shop',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FordShopPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController,
+    public navCtrl:NavController
+  ) { }
 
   ngOnInit() {
   }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Compra realizada com sucesso!',
+      duration: 2000,
+      color: 'success',
+      position: 'top',
+      cssClass: 'cssDoToast'
+    });
+    toast.present();
+  }
+
+  async presentAlert() {  
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Deseja realizar a compra?',
+      buttons: [
+        {
+          text: 'Não'
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            this.navCtrl.navigateRoot('/main');
+            this.presentToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+  
 }
